b50e42af36b37a884e3db6bd379f50ccf72a42a3
Ryan Gooler <cheetahmorph@gmail.com>
2012-06-21 23:25:11 -0700

    203 complete.

--- about_classes.rb
+++ about_classes.rb
@@ -149,7 +149,7 @@ class AboutClasses < EdgeCase::Koan
     end
 
     def to_s
-      self.__
+      self.name
     end
 
     def inspect
@@ -166,7 +166,7 @@ class AboutClasses < EdgeCase::Koan
 
   def test_to_s_provides_a_string_version_of_the_object
     fido = Dog7.new("Fido")
-    assert_equal __, fido.to_s
+    assert_equal "Fido", fido.to_s
   end
 
   def test_to_s_is_used_in_string_interpolation
