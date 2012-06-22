21955933c947193b014c890bd588be652f05d487
Ryan Gooler <cheetahmorph@gmail.com>
2012-06-21 22:54:44 -0700

    199 complete.

--- about_classes.rb
+++ about_classes.rb
@@ -101,7 +101,7 @@ class AboutClasses < EdgeCase::Koan
     fido = Dog5.new
 
     fido.name = "Fido"
-    assert_equal __, fido.name
+    assert_equal "Fido", fido.name
   end
 
   # ------------------------------------------------------------------
@@ -115,7 +115,7 @@ class AboutClasses < EdgeCase::Koan
 
   def test_initialize_provides_initial_values_for_instance_variables
     fido = Dog6.new("Fido")
-    assert_equal __, fido.name
+    assert_equal "Fido", fido.name
   end
 
   def test_args_to_new_must_match_initialize
